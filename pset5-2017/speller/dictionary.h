b9e0a066f5552618282b53579719c0452ae5eda6
julieyizhou <julie.yi.zhou@gmail.com>
2017-03-20 12:48:13 -0700

    Misspellings and dictionary

---
+++ pset5-2017/speller/dictionary.h
@@ -0,0 +1,47 @@
+/**
+ * Declares a dictionary's functionality.
+ */
+
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+
+#include <stdbool.h>
+
+// maximum length for a word
+// (e.g., pneumonoultramicroscopicsilicovolcanoconiosis)
+#define LENGTH 45
+
+// Structure for a node for a trie
+typedef struct node
+{
+    bool is_word;
+    struct node* children[27];
+}
+node;
+
+/**
+ * Returns true if word is in dictionary else false.
+ */
+bool check(const char *word);
+
+/**
+ * Loads dictionary into memory. Returns true if successful else false.
+ */
+bool load(const char *dictionary);
+
+/**
+ * Returns number of words in dictionary if loaded else 0 if not yet loaded.
+ */
+unsigned int size(void);
+
+/**
+ * Unloads dictionary from memory.  Returns true if successful else false.
+ */
+bool unload(void);
+
+/**
+ * Helper function for unloading dictionary from memory. Returns true if successful else false.
+ */
+bool free_trie_node(struct node *node);
+
+#endif // DICTIONARY_H
